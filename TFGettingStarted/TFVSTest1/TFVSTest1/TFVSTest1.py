e36b4ab3a58d59bff5c557d8dd4976fa812156d2
kamranthomas <kalima15@student.aau.dk>
2017-10-26 15:23:01 +0200

    Merge branch 'master' into branch-kamy
    # Conflicts:
    #	LATEX template/master.log
    #	LATEX template/master.pdf

---
+++ TFGettingStarted/TFVSTest1/TFVSTest1/TFVSTest1.py
@@ -0,0 +1,39 @@
+import tensorflow as tf
+
+node1 = tf.constant(3.0, dtype=tf.float32)
+node2 = tf.constant(4.0) #also tf.float32 implicitly
+print(node1,node2)
+
+sess = tf.Session()
+print(sess.run([node1,node2]))
+
+node3 = tf.add(node1,node2)
+print("node3: ",node3)
+print("sess.run(node3):", sess.run(node3))
+
+a = tf.placeholder(tf.float32)
+b = tf.placeholder(tf.float32)
+adder_node = a + b   #shortcut for tf.add(a,b)
+
+print(sess.run(adder_node, {a: 3, b:4.5}))
+print(sess.run(adder_node, {a: [1,3], b:[2,4]}))
+
+y=tf.placeholder(tf.float32)
+#add_and_triple = adder_node * 3.
+add_and_triple = adder_node * y
+print(sess.run(add_and_triple, {a: 3, b: 4.5, y: 3}))
+
+#variables
+W = tf.Variable([.3], dtype=tf.float32)
+b = tf.Variable([-.3], dtype=tf.float32)
+x = tf.placeholder(tf.float32)
+linear_model = W*x+b
+
+#variables are only initialized when you call:
+init = tf.global_variables_initializer()
+sess.run(init)
+#variab are still not initialized until you call sess.run(init)
+#init is a hande to the TensorFLow sub-graph that initialized
+#all the global variables
+
+print(sess.run(linear_model, {x: [1, 2, 3, 4]}))
