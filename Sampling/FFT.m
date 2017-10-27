856d37fba78b5c3231ff1a86377b5af129434c8a
stefanbirs <31699407+stefanbirs@users.noreply.github.com>
2017-10-27 11:42:50 +0200

    Pull Request

---
+++ Sampling/FFT.m
@@ -0,0 +1,24 @@
+%--------------------------------------------------------
+%Loading the "Hello" sample
+[y,Fs] = audioread('microphone-results.wav');
+sound(y,Fs);
+figure ();
+plot(y);
+
+%--------------------------------------------------------
+%Computing the FFT
+N = length(y);
+NEFT = 2^nextpow2(N)
+Xk=fft(y, NEFT)/N;                % perform N-point DFT of signal
+fa = 8000/2*linspace(0,1,NEFT/2+1);
+figure();
+plot(fa, 2*abs(Xk(1:NEFT/2+1)));
+title('FFT Spectrum Estimates / Power Spectral Density');
+xlabel('Frequency Index'),
+ylabel('Amplitude');
+
+%--------------------------------------------------------
+%Creating the Spectrogram
+figure();
+s = spectrogram(y, 25);
+spectrogram(y,'yaxis')
