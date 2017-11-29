67264cd6644d206dca77ef3a5ff75e0196f6bba2
stefanbirs <31699407+stefanbirs@users.noreply.github.com>
2017-11-29 17:09:03 +0100

    Check up
    Added CTC, Finished Classical approach, fixed equations, added App C (Matlab)

--- coding/MatLab/Sampling/FFT_sample.m
+++ coding/MatLab/Sampling/FFT_sample.m
@@ -1,20 +1,21 @@
 %--------------------------------------------------------
 %Loading the "Hello" sample
+subplot(3,1,1);
 [y,Fs] = audioread('microphone-results.wav');
 sound(y,Fs);
     dt = 1/Fs;
     t = 0:dt:(length(y)*dt)-dt;
-    figure();
+    %figure();
     plot(t,y); xlabel('Seconds'); ylabel('Amplitude');
-
+title('Hello waveform');
 
 %--------------------------------------------------------
-
+subplot(3,1,2);
 N = length(y);
 NEFT = 2^nextpow2(N)
 Xk=fft(y, NEFT)/N;                % perform N-point DFT of signal
 fa = 8000/2*linspace(0,1,NEFT/2+1); 
-figure();
+%figure();
 plot(fa, 2*abs(Xk(1:NEFT/2+1)));
 title('FFT Spectrum Estimates / Power Spectral Density');
 xlabel('Frequency Index'),
@@ -23,7 +24,8 @@ ylabel('Amplitude');
 %%%%%--------------------------------------------------------
 % % % % % Creating the Spectrogram
 figure();
+%subplot(3,1,3);
 s = spectrogram(y);
-spectrogram(y,'yaxis')
+spectrogram(y,25,'yaxis')
 
 
